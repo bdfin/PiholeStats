5be1d49527760fac5843154f6d90a5abf4e0494c
Beau Findlay <37879824+bdfin@users.noreply.github.com>
2020-03-06 15:53:13 +0000

    Initial code commit.

---
+++ PiholeStats.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PiholeStats.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
